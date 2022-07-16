5c41f947b45a9e99a0a160d13f25e6b04f2cc6b8
Mohamed  Ali <48335933+kahano@users.noreply.github.com>
2022-07-16 14:15:28 +0200

    Add files via upload

---
+++ Hovedprogram.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Hovedprogram{
+  public static void main(String [] args) throws Exception{
+    Salgsystem salgSys = new Salgsystem();
+    salgSys.leseFrafil("kunde_produkter.txt");
+    salgSys.ordre_lokke();
+    salgSys.skrivTilFil("kunde_produkter.txt");
+  }
+}
